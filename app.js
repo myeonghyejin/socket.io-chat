dfebdcb13d15dcc16fa4c6037c5449d84de35c76
myeonghyejin <myeonghyejin@icloud.com>
2023-12-29 14:46:36 +0900

    backup

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ connection.connect();
 const multer = require('multer');
 const uuid4 = require('uuid4');
 const path = require('path');
-const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -50,17 +49,9 @@ const upload = multer({
 /* 이미지 저장 함수들 */
 async function saveImageToDB(imagePath) {
     try {
-        await connection.query("INSERT INTO chat_image (image_path) VALUES (?)", [imagePath]);
-        console.log('이미지 경로를 데이터베이스에 저장했습니다.');
-    } catch (error) {
-        throw new Error('이미지 경로를 데이터베이스에 저장하는 중 오류가 발생했습니다.');
-    }
-}
-
-async function saveImageToDB(imagePath) {
-    try {
         const result = await connection.query("INSERT INTO chat_image (image_path) VALUES (?)", [imagePath]);
-        console.log('이미지 경로를 데이터베이스에 저장했습니다.', result);
+        console.log('이미지 경로를 데이터베이스에 저장했습니다.');
+        return result;
     } catch (error) {
         console.error('이미지 경로를 데이터베이스에 저장하는 중 오류가 발생했습니다:', error);
         throw new Error('이미지 경로를 데이터베이스에 저장하는 중 오류가 발생했습니다.');
@@ -83,7 +74,8 @@ app.post('/upload', upload.array('imgs', 10), async (req, res) => {
             await saveImageToDB(imagePath);
         }
 
-        res.status(200).send('이미지 업로드 완료');
+        // 이미지 업로드가 완료되면 이미지 경로를 클라이언트에 응답으로 보내줌
+        res.status(200).json({ imagePaths });
     } catch (err) {
         console.error('이미지 업로드 중 오류 발생:', err);
         res.status(500).send('이미지 업로드 중 오류 발생');
